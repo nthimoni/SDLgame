5834beaa3204e058f513fc5d4f0a10763b372440
nthimoni <thimoniern@gmail.com>
2021-09-05 16:08:30 +0200

    modif test init sdl

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,10 @@ int	main(int argc, char *argv[])
 {
 	(void)argc;
 	(void)argv;
-	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 		std::cout << "SDL_init has failed. SDL_ERROR : " << SDL_GetError() << std::endl;
-	if (!(IMG_Init(IMG_INIT_PNG)))
+	if (!IMG_Init(IMG_INIT_PNG))
 		std::cout << "IMG_init has failed. SDL_ERROR : " << SDL_GetError() << std::endl;
-
 
 	return (0);
 }
