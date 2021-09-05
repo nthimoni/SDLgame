3ecf748048032bb8739f1c871d985a9c7fd87e88
nthimoni <thimoniern@gmail.com>
2021-09-05 12:17:18 +0200

    okok

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ int	main(int argc, char *argv[])
 	(void)argc;
 	(void)argv;
 	if (SDL_Init(SDL_INIT_VIDEO) > 0)
-		std::cout << "SDL_init has failed FDP. SDL_ERROR : " << SDL_GetError() << std::endl;
-	if (!(IMG_Init(IMG_INIT_PNG)))
 		std::cout << "SDL_init has failed. SDL_ERROR : " << SDL_GetError() << std::endl;
+	if (!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG_init has failed. SDL_ERROR : " << SDL_GetError() << std::endl;
 
 
 	return (0);
